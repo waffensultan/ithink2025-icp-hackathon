0cdc5aa661a5cec9f82cbfcd3c6a4320871dea35
Waffen Sultan <waffen.sultan@gmail.com>
2025-06-06 22:20:21 +0800

    feat: completed farmer and buyer profile

---
+++ frontend/src/app/buyer/profile/wallet/page.tsx
@@ -0,0 +1,27 @@
+import { WalletIcon, CircleDollarSignIcon as DollarIcon } from "lucide-react";
+
+export default function Page() {
+    return (
+        <main className="px-4 text-primary py-5">
+            <div className="flex  flex-col gap-1 pb-3">
+                <div className="flex items-center gap-2 text-primary text-3xl">
+                    <WalletIcon size={30} />
+                    <h1 className="font-bold">Wallet</h1>
+                </div>
+            </div>
+            <article className="border-2 border-primary rounded-2xl flex flex-col gap-1 p-4">
+                <div className="flex justify-between items-start pb-4">
+                    <DollarIcon size={50} />
+                    <img src="/profile/wallet/icp.svg" alt="icp-icon" />
+                </div>
+                <div className="flex flex-col">
+                    <div className="flex items-start gap-2">
+                        <h4 className="font-bold text-2xl">12,100.75 PHP</h4>
+                        <span className="opacity-50 text-lg">41.42 ICP</span>
+                    </div>
+                    <span className="opacity-50 -mt-1">Kita</span>
+                </div>
+            </article>
+        </main>
+    );
+}
--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -2,15 +2,21 @@
 
 import Link from "next/link";
 
+import { ShoppingCartIcon } from "lucide-react";
+
 export default function Nav() {
     return (
         <nav className="flex items-center justify-between p-3">
             <section className="flex items-center gap-1">
                 <Link href={"/"}>
-                    <h2 className="text-primary font-bold text-2xl">FarmChain</h2>
+                    <h2 className="text-primary font-bold text-2xl">
+                        FarmChain
+                    </h2>
                 </Link>
             </section>
-            <button className="bg-primary rounded-md py-2 px-3 font-bold text-light">My Wallet</button>
+            <button className="bg-primary rounded-md py-2 px-3 font-bold text-light">
+                <ShoppingCartIcon />
+            </button>
         </nav>
     );
 }
